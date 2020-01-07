5a99f43d3551bf46a96f9a05b6cf4d7b4538b158
JeroneHeuveling <57947315+JeroneHeuveling@users.noreply.github.com>
2020-01-07 10:15:15 +0100

    Arduino code updated

---
+++ TaalGame/Messages.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace TaalGame
+{
+    class Messages
+    {
+        public string extractedData { get; private set; } = "";
+        private string incomingData { get; set; } = "";
+        public bool receivingData { get; set; } = true;
+        public Messages(SerialPort poort)
+        {
+            poort.Open();
+        }
+        public void Receive(SerialPort poort)
+        {
+            bool messageHandled = false;
+            int firstindex;
+            int lastindex;
+            if (messageHandled == false)
+            {
+                incomingData = "";
+                incomingData += poort.ReadLine();
+                firstindex = incomingData.IndexOf('>');
+                lastindex = incomingData.IndexOf('<');
+                for (int x = firstindex + 1; x < lastindex; x++)
+                {
+                    extractedData += incomingData[x];
+
+                }
+                messageHandled = true;
+
+            }
+
+
+        }
+        public void clearIncomingData()
+        {
+            extractedData = "";
+            incomingData = "";
+        }
+    }
+}
+
+
+
